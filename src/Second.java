87af9fb1efa82df0d88fd6cac067ce055d9bd931
Darya <dasha27061999@mail.ru>
2018-02-26 20:47:33 +0300

    26.02(part3)

--- src/Second.java
+++ src/Second.java
@@ -13,13 +13,9 @@ import javax.swing.border.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-
+import java.util.*;
 public class Second extends JFrame{
 
-	public JButton button = new JButton ("ADD");
-	public JTextField input = new JTextField ("", 25);
-	public String[] items = {""};
-	public JComboBox comboBox = new JComboBox(items);
 	public Second()
 	{
 		setTitle("My window");
@@ -27,33 +23,55 @@ public class Second extends JFrame{
 		setLayout(null);
 		setLocationByPlatform(true);  //расположение на экране
 		
+		JButton button = new JButton ("ADD");
+		JTextField input = new JTextField ("", 25);
+		Vector<String> items = new Vector<String>();
+		items.addElement("Hello");
+		items.addElement("Bye");
+		//String[] items = {""};
+		JComboBox comboBox = new JComboBox(items);
+		
 		comboBox.setEditable(true); //чтобы можно было редактировать
 		
 		comboBox.setBounds(45,40,150,25);
-		getContentPane().add(comboBox);	    
+		add(comboBox);	   //getContentPane(). 
 	    
 	    input.setBounds(45,10,150,25);
-	    getContentPane().add(input);
+	    add(input); 
 	    
 	    button.setBounds(70,90,100,40);
-		getContentPane().add(button);
+		add(button);
 		
-		button.addActionListener(new ListenerAction());
-	}
-	
-	class ListenerAction implements ActionListener{
-		public void actionPerformed(ActionEvent event)
-		{
+		//public void bAddActionPerformed(java.awt.event.ActionEvent event) {
 			
-			
-		}
-
+		//внутри - исполнитель этого события
+		button.addActionListener(new ActionListener()
+				{
+			public void actionPerformed(ActionEvent event)
+			{
+				String text = input.getText();	
+				
+				for (int index = 0; index < items.size(); index++)
+				{
+					if (items.get(index) != text && index == items.size()-1)
+					{
+						comboBox.addItem(text);
+						System.out.println("Add");
+					}
+				}
+				/*for (int index = 0; index < items.size(); index++)
+				{
+					if (items.get(index) == text)
+						{System.out.println("Element exist!!!");}						
+					else if (index == items.size()-1)
+					{System.out.println("Add");*/
+						
+						//}						
+				//}
+				
+			}
+				});		
 	}
-
-	
-
-
-	
 }
 
 
